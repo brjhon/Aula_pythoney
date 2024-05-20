b908d7d54d425eee5e2a9e34a59e180228da3ab7
jhonwesley <102744615+brjhon@users.noreply.github.com>
2024-05-20 20:14:31 -0300

    secund-atualização-upgrade

--- Pythone/Programação font-End/Aula-6/script.js
+++ Pythone/Programação font-End/Aula-6/script.js
@@ -1,6 +1,6 @@
-const themeButton = document.getElementById('themeButton');
+/* const themeButton = document.getElementById('themeButton');
 const body = document.budy;
 
 themeButton.addEventListener('click', () => {
     body.classList.toggle('dark-theme');
-});
+}); */
