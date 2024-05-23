cc298f0d5999b352cd7b79ccd7f692127545ef67
jhonwesley <102744615+brjhon@users.noreply.github.com>
2024-05-23 20:10:48 -0300

    Atualização: Dever-em-python-1

---
+++ Pythone/Uc_3/Dever-1.py
@@ -0,0 +1,20 @@
+Print("Programa de avaiação de Carro")
+
+avaliação = int(input("Escolha qual e a Qualificação de seu carro, opções:/n 1: 0 a 50/n 2: 50 a 80/n 3: 80 a 100% /n escolha uma opão: "))
+Carro = int(input("Qual e o carro: "))
+
+if avaliação == 1:
+    print(f"A avaliação de seu {Carro} de 0% para 50%")
+    
+elif avaliação == 2:
+        print(f"A avaliação de seu {Carro} de 50% para 80%")
+
+elif avaliação == 3:
+        print(f"A avaliação de seu {Carro} de 80% para 100%")
+
+elif avaliação == 4:
+    break
+
+else:
+    print("Você escolheu uma opção invalida")
+break
