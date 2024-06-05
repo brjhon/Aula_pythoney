ab2aa5dcbbec314273f533769e6b5020df53e35a
jhonwesley <102744615+brjhon@users.noreply.github.com>
2024-06-04 23:24:53 -0300

    Batalha naval

--- Pythone/Uc_3/Alua-5/Batalha-naval.py
+++ Pythone/Uc_3/Alua-5/Batalha-naval.py
@@ -2,24 +2,60 @@
 #Deverá ser possível colocar submarinos, que são barcos que ocupam 1 de espaço na matriz inicialmente 
 #serão colocados de maneira aleatória e o usuário não saberá onde ficarão posicionados.
 
-import random
-def verificar_tesouro(indici, locais):
-    if locais[indici] == 1:
-        return(True)
-    
+#Inicio no codigo
+from random import randint
+
+# ---- Confuguração do tabuleiro ----
+
+board_size = 5  # Tamanho do tabuleiro (5x5 neste exemplo)
+board = [['O' for _ in range(board_size)] for _ in range(board_size)] # Nesta lina: E a criação do tabuleiro como uma matriz bidimensional usando listas de compreensão.
+#Cada celula do tabuleiro e inicializada com '0', informando água.
+
+def print_board(board):
+    for row in board:
+        print(' '.join(row))
+
+print_board(board)
+                                          # ---- PARTE LEGAL ----
+# ---- Posicionamento dos Navios. ----
+def place_ship(board):
+    ship_row = randint(0, board_size - 1)#Gera aleatoriamente um número inteiro entre 0 e board_size - 1, que representa a linha onde o navio será posicionado.
+    ship_col = randint(0, board_size - 1)
+    board[ship_row][ship_col] = 'X' #Define a célula do tabuleiro na posição gerada aleatoriamente
+                                    #como 'X'
+    return ship_row, ship_col # Retorna as cordenadas (Linha/Coluna)
+
+ship_row, ship_col = place_ship(board) #Posiciona o navio e obtem as coordenadas dele.
+print_board(board)
+#Imprime o tabuleiro atualizado.
+
+
+# ---- Jogada do jogador. ----
+def player_move(board):
+    while True:
+        try:
+            row = int(input("Informe a linha (0-4): "))
+            col = int(input("Informe a coluna (0-4): "))
+            if 0 <= row < board_size and 0 <= col < board_size:
+                return row, col
+            else:
+                print("Por favor, insira valores dentro do intervalo permitido.")
+        except ValueError:
+            print("Por favor, insira valores numéricos.")
+
+player_row, player_col = player_move(board)
+
+# ---- Verificação da Jogada ----
+def check_move(board, row, col):
+    if board[row][col] == 'X':
+        print("Parabéns! Você acertou o navio!")
+        board[row][col] = '!'
     else:
-        return (False)
-    
+        print("Você errou. Tente novamente.")
+        board[row][col] = '.'
+
+check_move(board, player_row, player_col)
+print_board(board)
 
 
-def main():
-    print("Bem vindo ao jogo de batalha naval!")
-    print("Escolha um número entre 1 e 10 para procurar o tesouro.")
-
-    # Define o número de tentativas 
-    tentativas = 3
-    print("Tente encontrar o tesouro. Voce tem 3 tentativas. ")
-    print("Escolha um número entre 0 e 9 para procurar o tesouro. ")
-    #Inicializa o contador de tentativas 
-    contador = 0
 
