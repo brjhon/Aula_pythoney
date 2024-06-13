2dccdb81d77924873d407defc8e349807f598868
jhonwesley <102744615+brjhon@users.noreply.github.com>
2024-06-12 21:31:22 -0300

    Mat-TurmaPython

---
+++ Pythone/SQL/MAt-Turma.sql
@@ -0,0 +1,13 @@
+#INSERT INTO turmapython (nome, Ultimonome, Telefone, Sexo) VALUES
+#('Jhon', 'Wesley', '967311584', 'Masculino'),
+#('Igor', 'Clerigo', '999999999', 'Masculino'),
+#('Cauão', 'Santos', '999999999', 'Masculino'),
+#('Lore', 'Resis', '999999999', 'Feminino'),
+#('Kauão', 'Luis', '999999999', 'Masculino'),
+#('Gabriel', 'Costas', '999999999', 'Masculino'),
+#('Ale', 'Dias', '999999999', 'Masculino'),
+#('Igor2', 'Barbosa', '999999999', 'Masculino')
+
+#select  nome from turmapython where Sexo = 'Feminino'   
+
+#select * from turmapython
